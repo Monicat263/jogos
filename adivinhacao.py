c4b47c089eaf4dbc1beb4101413eaeec0b4897d4
monicat <mda263@bol.com.br>
2023-02-24 23:19:13 -0300

    Implentando jogo com utilização de if,else e elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,6 +1,5 @@
 #Jogo de adivinhação
 #Regras: O usuário digitará um número e o programa retornará se ele acertou ou não este número
-
 #Definir um número qualquer
 numero_secreto = 42
 
@@ -8,12 +7,22 @@ numero_secreto = 42
 chute_do_usuario = input ('Digite seu número:')
 print("Você digitou: ", chute_do_usuario )
 #Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
-chute_convertido_utilizando_casting = int(chute_do_usuario)
+chute_convertido_utilizando_cast = int(chute_do_usuario)
 
-if(numero_secreto == chute_convertido_utilizando_casting):
+acertou_numero_secreto = numero_secreto == chute_convertido_utilizando_cast
+chute_maior_que_numero_secreto = chute_convertido_utilizando_cast > numero_secreto
+chute_mmenor_que_numero_secreto = chute_convertido_utilizando_cast < numero_secreto
+
+if(acertou_numero_secreto):
      print('Parabéns!!! Você acertou o número secreto')
 else:
-    print('Você não acertou o número secreto')
+    if(chute_maior_que_numero_secreto):
+        print("Você errou!!! Dica: O número digitado é maior que o número secreto")
+    elif(chute_mmenor_que_numero_secreto):
+        print("Você errou!!! Dica: O número digitado é menor que o número secreto")
+
 print('## Fim do jogo ##')
+
+
 
 

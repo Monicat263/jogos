a522991e5c91e4955296a82b85792f6a9a68923a
monicat <mda263@bol.com.br>
2023-02-26 09:37:32 -0300

    implementando o código utilizando laço de repetição while

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,24 +1,40 @@
 # Jogo de adivinhação
 # Regras: O usuário digitará um número e o programa retornará se ele acertou ou não este número
 # Definir um número qualquer
+
+
+print('########################################################################################')
+print('Bem vindo(a) ao jogo de adivinhação, você terá 3 tentativas para acerta o número secreto')
+print('                                     Boa sorte!!!')
+print('########################################################################################')
+
 numero_secreto = 42
+max_tentativas = 3
+rodada = 1
 
-# função que permite que o usuário digite um valor no console
-chute_do_usuario = input("Digite seu número:")
-print("Você digitou: ", chute_do_usuario)
-# Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
-chute_convertido_utilizando_cast = int(chute_do_usuario)
+while(rodada <= max_tentativas):
 
-acertou_numero_secreto = numero_secreto == chute_convertido_utilizando_cast
-chute_maior_que_numero_secreto = chute_convertido_utilizando_cast > numero_secreto
-chute_mmenor_que_numero_secreto = chute_convertido_utilizando_cast < numero_secreto
+    print('Tentativas {0} de {1}'.format(rodada,max_tentativas))
+    # função que permite que o usuário digite um valor no console
+    chute_do_usuario = input("Digite seu número:")
+    print('Você digitou: ', chute_do_usuario)
+    # Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
+    chute_convertido_utilizando_cast = int(chute_do_usuario)
 
-if acertou_numero_secreto:
-    print("Parabéns!!! Você acertou o número secreto")
-else:
-    if chute_maior_que_numero_secreto:
-        print("Você errou!!! Dica: O número digitado é maior que o número secreto")
-    elif chute_mmenor_que_numero_secreto:
-        print("Você errou!!! Dica: O número digitado é menor que o número secreto")
+    acertou_numero_secreto = chute_convertido_utilizando_cast == numero_secreto
+    chute_maior_que_numero_secreto = chute_convertido_utilizando_cast > numero_secreto
+    chute_menor_que_numero_secreto = chute_convertido_utilizando_cast < numero_secreto
+
+    if acertou_numero_secreto:
+        print('Parabéns!!! Você acertou o número secreto')
+        break
+    else:
+        if chute_maior_que_numero_secreto:
+            print('Você errou!!! Dica: O número digitado é maior que o número secreto')
+        elif chute_menor_que_numero_secreto:
+            print('Você errou!!! Dica: O número digitado é menor que o número secreto')
+
+    rodada = rodada  + 1
+
 
 print("## Fim do jogo ##")

fdb0a2aee98b203fb4361eac339571447df3f13d
monicat <mda263@bol.com.br>
2023-03-02 20:16:30 -0300

    Incluindo seleção referente ao nivel de dificuldade do jogo

--- adivinhacaoWhile.py
+++ adivinhacaoWhile.py
@@ -12,6 +12,32 @@ numero_secreto = random.randrange(1,101)
 max_tentativas = 3
 rodada = 1
 
+#Escolha do usuário para nivel do jogo
+print('Selecione o nivel de dificuldade do jogo: ')
+print('(1) Fácil')
+print('(2) Médio')
+print('(3) Dificil')
+# variavel recebendo a input do usuário(String) e já convertendo para int
+nivel = int(input('Defina o nível: '))
+
+
+while(nivel < 1 or nivel > 3 ):
+    print("ESSE NÍVEL NÃO EXISTE, INSIRA UM NÍVEL VALIDO!\n")
+    print("Escolha a dificuldade.")
+    print("(1) Fácil | (2) Normal | (3) Difícil\n")
+    nivel= int(input("Dificuldade: "))
+
+    nivel_facil = nivel == 1
+    nivel_medio = nivel == 2
+    nivel_dificil = nivel == 3
+
+    if(nivel_facil):
+        max_tentativas = 8
+    elif(nivel_medio):
+        max_tentativas = 5
+    elif(nivel_dificil):
+        max_tentativas = 3
+
 while(rodada <= max_tentativas):
     print('Tentativas {0} de {1}'.format(rodada,max_tentativas))
  # função que permite que o usuário digite um valor no console

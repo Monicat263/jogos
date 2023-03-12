c4d09baacf07b1472c6fa4ebc76dd3c6f9df21c4
monicat <mda263@bol.com.br>
2023-03-12 00:40:32 -0300

    Refatorando classe jogos para melhorar legibilidade do código e adicionando funções para execução dos jogo selecionado

--- jogos.py
+++ jogos.py
@@ -1,24 +1,29 @@
+# Classe principal responsavel por disponibilizar as opções de jogos para o o usuário selecionar#
+import adivinhacaoFor
+import adivinhacaoWhile
+import forca
+def jogar_forca():
+    print('########################################################################################')
+    print('Bem vindo(a) ao mundo python, escolha o seu jogo')
+    print('                                     Boa sorte!!!')
+    print('########################################################################################')
 
+#opções de jogos
+print('(1) Forca (2) Adivinhação While (3) Adivinhação For')
 
-print('########################################################################################')
-print('Bem vindo(a) ao mundo python, escolha o seu jogo')
-print('                                     Boa sorte!!!')
-print('########################################################################################')
-
-
-print('(1) Adivinhação For (2) Adivinhação While (3) Forca')
-
+#conversão resposta do usuário de String para int
 resp_usuario = int(input("Selecione a opção do jogo: "))
-adivinhacao_for = resp_usuario == 1
-adivinhacao_while = resp_usuario == 2
-forca = resp_usuario == 3
 
 if (resp_usuario ==1):
-    print('você selecionou a opção adivinhação For')
+    print('você selecionou a opção Forca')
+    # chamando a função da classes
+    forca.jogar()
 elif(resp_usuario ==2):
     print('você selecionou a opção adivinhação While')
+    adivinhacaoWhile.jogar_adivinhacao_while()
 elif (resp_usuario==3):
-    print('você selecionou a opção adivinhação forca')
+    print('você selecionou a opção adivinhação adivinhação For')
+    adivinhacaoFor.jogar_adivinhacao_for()
 else:
      print('opção inválida')
 

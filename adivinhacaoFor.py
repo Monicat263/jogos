d2827b26c35eb0dfc9fb6772b4414e9c662bc4e0
monicat <mda263@bol.com.br>
2023-03-12 00:41:56 -0300

    Criando novas classes

--- adivinhacaoFor.py
+++ adivinhacaoFor.py
@@ -5,71 +5,73 @@
 
 import random
 
-print('########################################################################################')
-print('Bem vindo(a) ao jogo de adivinhação, você terá 3 tentativas para acerta o número secreto')
-print('                                     Boa sorte!!!')
-print('########################################################################################')
+def jogar_adivinhacao_for():
 
-#Inicialização de variáveis
-numero_secreto = random.randrange(1, 101)
-max_tentativas = 0
-pontos = 1000
+    print('########################################################################################')
+    print('Bem vindo(a) ao jogo de adivinhação, você terá 3 tentativas para acerta o número secreto')
+    print('                                     Boa sorte!!!')
+    print('########################################################################################')
 
-# Escolha do usuário para nivel do jogo
-print('Selecione o nivel de dificuldade do jogo: ')
-print('(1) Fácil (2) Médio (3) Dificil')
-# variavel recebendo a input do usuário(String) e já convertendo para int
-nivel = int(input('Defina o nível: '))
+    #Inicialização de variáveis
+    numero_secreto = random.randrange(1, 101)
+    max_tentativas = 0
+    pontos = 1000
 
-# for n in range(0,nivel + 1):
-#     nivel_incorreto  = input("Digite um número entre 1 e 3: ")
-#     if (nivel < 1 or nivel > 3):
-#         continue
-nivel_facil = nivel == 1
-nivel_medio = nivel == 2
-nivel_dificil = nivel == 3
+    # Escolha do usuário para nivel do jogo
+    print('Selecione o nivel de dificuldade do jogo: ')
+    print('(1) Fácil (2) Médio (3) Dificil')
+    # variavel recebendo a input do usuário(String) e já convertendo para int
+    nivel = int(input('Defina o nível: '))
 
-if nivel_facil:
-    max_tentativas = 8
-if nivel_medio:
-    max_tentativas = 5
-if nivel_dificil:
-     max_tentativas = 3
+    # for n in range(0,nivel + 1):
+    #     nivel_incorreto  = input("Digite um número entre 1 e 3: ")
+    #     if (nivel < 1 or nivel > 3):
+    #         continue
+    nivel_facil = nivel == 1
+    nivel_medio = nivel == 2
+    nivel_dificil = nivel == 3
 
-for rodada in range(1, max_tentativas + 1):
-    print('Tentativas {} de {}'.format(rodada, max_tentativas))
+    if nivel_facil:
+        max_tentativas = 8
+    if nivel_medio:
+        max_tentativas = 5
+    if nivel_dificil:
+         max_tentativas = 3
 
-    # função que permite que o usuário digite um valor no console
-    chute_do_usuario = input("Digite um número entre 1 e 100: ")
-    print('Você digitou: ', chute_do_usuario)
+    for rodada in range(1, max_tentativas + 1):
+        print('Tentativas {} de {}'.format(rodada, max_tentativas))
 
-    # Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
-    chute_convertido_utilizando_casting = int(chute_do_usuario)
-    # Verificação se o número digitado está entre 1 ou 100
-    if (chute_convertido_utilizando_casting < 1 or chute_convertido_utilizando_casting >= 100):
-        continue
+        # função que permite que o usuário digite um valor no console
+        chute_do_usuario = input("Digite um número entre 1 e 100: ")
+        print('Você digitou: ', chute_do_usuario)
 
-    acertou_numero_secreto = chute_convertido_utilizando_casting == numero_secreto
-    chute_maior_que_numero_secreto = chute_convertido_utilizando_casting > numero_secreto
-    chute_menor_que_numero_secreto = chute_convertido_utilizando_casting < numero_secreto
+        # Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
+        chute_convertido_utilizando_casting = int(chute_do_usuario)
+        # Verificação se o número digitado está entre 1 ou 100
+        if (chute_convertido_utilizando_casting < 1 or chute_convertido_utilizando_casting >= 100):
+            continue
 
-    if acertou_numero_secreto:
-        print('Parabéns!!! Você acertou o número secreto é: {} e fez {} pontos'.format(numero_secreto,pontos))
-        break
-    else:
-        if chute_maior_que_numero_secreto:
-            print('Você errou!!! Dica: O número digitado é maior que o número secreto')
-            if (rodada == max_tentativas):
-                print("O número secreto era {}. Você fez {}".format(
-                    numero_secreto, pontos))
-        elif chute_menor_que_numero_secreto:
-            print('Você errou!!! Dica: O número digitado é menor que o número secreto')
-            if (rodada == max_tentativas):
-                print("O número secreto era {}. Você fez {}".format(
-                    numero_secreto, pontos))
-        #Cálculo pontos perdidos. Utilizada abs para eliminar a diferenças de sinais 
-        pontos_perdidos = abs(numero_secreto - chute_convertido_utilizando_casting)
-        pontos = pontos - pontos_perdidos
-        print(pontos)
+        acertou_numero_secreto = chute_convertido_utilizando_casting == numero_secreto
+        chute_maior_que_numero_secreto = chute_convertido_utilizando_casting > numero_secreto
+        chute_menor_que_numero_secreto = chute_convertido_utilizando_casting < numero_secreto
 
-print('## Fim do jogo ##')
+        if acertou_numero_secreto:
+            print('Parabéns!!! Você acertou o número secreto é: {} e fez {} pontos'.format(numero_secreto,pontos))
+            break
+        else:
+            if chute_maior_que_numero_secreto:
+                print('Você errou!!! Dica: O número digitado é maior que o número secreto')
+                if (rodada == max_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
+            elif chute_menor_que_numero_secreto:
+                print('Você errou!!! Dica: O número digitado é menor que o número secreto')
+                if (rodada == max_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
+            #Cálculo pontos perdidos. Utilizada abs para eliminar a diferenças de sinais
+            pontos_perdidos = abs(numero_secreto - chute_convertido_utilizando_casting)
+            pontos = pontos - pontos_perdidos
+            print(pontos)
+
+    print('## Fim do jogo ##')
--- adivinhacaoWhile.py
+++ adivinhacaoWhile.py
@@ -4,78 +4,78 @@
 # Pontos perdidos: É a diferença entre os número secreto - o valor digitado subtraido da pontuação inicial
 
 import random
+def jogar_adivinhacao_while():
+    print('########################################################################################')
+    print('Bem vindo(a) ao jogo de adivinhação, você terá 3 tentativas para acerta o número secreto')
+    print('                                     Boa sorte!!!')
+    print('########################################################################################')
 
-print('########################################################################################')
-print('Bem vindo(a) ao jogo de adivinhação, você terá 3 tentativas para acerta o número secreto')
-print('                                     Boa sorte!!!')
-print('########################################################################################')
+    numero_secreto = random.randrange(1,101)
+    max_tentativas = 3
+    rodada = 1
+    pontos = 1000
 
-numero_secreto = random.randrange(1,101)
-max_tentativas = 3
-rodada = 1
-pontos = 1000
+    #Escolha do usuário para nivel do jogo
+    print('Selecione o nivel de dificuldade do jogo: ')
+    print('(1) Fácil')
+    print('(2) Médio')
+    print('(3) Dificil')
+    # variavel recebendo a input do usuário(String) e já convertendo para int
+    nivel = int(input('Defina o nível: '))
+    print(numero_secreto)
 
-#Escolha do usuário para nivel do jogo
-print('Selecione o nivel de dificuldade do jogo: ')
-print('(1) Fácil')
-print('(2) Médio')
-print('(3) Dificil')
-# variavel recebendo a input do usuário(String) e já convertendo para int
-nivel = int(input('Defina o nível: '))
-print(numero_secreto)
+    while(nivel < 1 or nivel > 3 ):
+        print("ESSE NÍVEL NÃO EXISTE, INSIRA UM NÍVEL VALIDO!\n")
+        print("Escolha a dificuldade.")
+        print("(1) Fácil | (2) Normal | (3) Difícil\n")
+        nivel= int(input("Dificuldade: "))
 
-while(nivel < 1 or nivel > 3 ):
-    print("ESSE NÍVEL NÃO EXISTE, INSIRA UM NÍVEL VALIDO!\n")
-    print("Escolha a dificuldade.")
-    print("(1) Fácil | (2) Normal | (3) Difícil\n")
-    nivel= int(input("Dificuldade: "))
+        nivel_facil = nivel == 1
+        nivel_medio = nivel == 2
+        nivel_dificil = nivel == 3
 
-    nivel_facil = nivel == 1
-    nivel_medio = nivel == 2
-    nivel_dificil = nivel == 3
+        if(nivel_facil):
+            max_tentativas = 8
+        elif(nivel_medio):
+            max_tentativas = 5
+        elif(nivel_dificil):
+            max_tentativas = 3
 
-    if(nivel_facil):
-        max_tentativas = 8
-    elif(nivel_medio):
-        max_tentativas = 5
-    elif(nivel_dificil):
-        max_tentativas = 3
+    while(rodada <= max_tentativas):
+        print('Tentativas {0} de {1}'.format(rodada,max_tentativas))
+    # função que permite que o usuário digite um valor no console
+        chute_do_usuario = input("Digite um número entre 1 e 100:  ")
+    # Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
+        chute_convertido_utilizando_casting = int(chute_do_usuario)
 
-while(rodada <= max_tentativas):
-    print('Tentativas {0} de {1}'.format(rodada,max_tentativas))
-# função que permite que o usuário digite um valor no console
-    chute_do_usuario = input("Digite um número entre 1 e 100:  ")
-# Conversão da entrada do usuário de str(String) para int, assim consigo fazer a comparação
-    chute_convertido_utilizando_casting = int(chute_do_usuario)
+    # Verificação se o número digitado está entre 1 ou 100
+        if(chute_convertido_utilizando_casting < 1 or chute_convertido_utilizando_casting >= 100):
+            continue
+    # Declaração de variáveis com possiveis tipos de acerto
+        acertou_numero_secreto = chute_convertido_utilizando_casting == numero_secreto
+        chute_maior_que_numero_secreto = chute_convertido_utilizando_casting > numero_secreto
+        chute_menor_que_numero_secreto = chute_convertido_utilizando_casting < numero_secreto
 
-# Verificação se o número digitado está entre 1 ou 100
-    if(chute_convertido_utilizando_casting < 1 or chute_convertido_utilizando_casting >= 100):
-        continue
-# Declaração de variáveis com possiveis tipos de acerto
-    acertou_numero_secreto = chute_convertido_utilizando_casting == numero_secreto
-    chute_maior_que_numero_secreto = chute_convertido_utilizando_casting > numero_secreto
-    chute_menor_que_numero_secreto = chute_convertido_utilizando_casting < numero_secreto
+    # Testendo condições
+        if acertou_numero_secreto:
+            print('Parabéns!!! Você acertou o número secreto é: {} e você fez {} pontos'.format(numero_secreto,pontos))
+            break
+        else:
+            if chute_maior_que_numero_secreto:
+                print('Você errou!!! Dica: O número digitado é maior que o número secreto')
+                if (rodada == max_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
+            elif chute_menor_que_numero_secreto:
+                print('Você errou!!! Dica: O número digitado é menor que o número secreto')
+                if (rodada == max_tentativas):
+                    print("O número secreto era {}. Você fez {}".format(
+                        numero_secreto, pontos))
 
-# Testendo condições
-    if acertou_numero_secreto:
-        print('Parabéns!!! Você acertou o número secreto é: {} e você fez {} pontos'.format(numero_secreto,pontos))
-        break
-    else:
-        if chute_maior_que_numero_secreto:
-            print('Você errou!!! Dica: O número digitado é maior que o número secreto')
-            if (rodada == max_tentativas):
-                print("O número secreto era {}. Você fez {}".format(
-                    numero_secreto, pontos))
-        elif chute_menor_que_numero_secreto:
-            print('Você errou!!! Dica: O número digitado é menor que o número secreto')
-            if (rodada == max_tentativas):
-                print("O número secreto era {}. Você fez {}".format(
-                    numero_secreto, pontos))
-
-        # Regra referente a pontos perdidos
-    pontos_perdidos = abs(numero_secreto - chute_convertido_utilizando_casting)
-    pontos = pontos - pontos_perdidos
-    print(pontos)
-    rodada = rodada +1
+            # Regra referente a pontos perdidos
+        pontos_perdidos = abs(numero_secreto - chute_convertido_utilizando_casting)
+        pontos = pontos - pontos_perdidos
+        print(pontos)
+        rodada = rodada +1
 
 print("## Fim do jogo ##")

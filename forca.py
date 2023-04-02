96406bea4dd19a5ad091f7c2596b15e2c18e9466
monicat <mda263@bol.com.br>
2023-04-02 09:53:57 -0300

    Alterando classe forca para gerar lista com palavra secreta aleatoriamente e número de tentativas igual ao cumprimento da palavra secreta

--- forca.py
+++ forca.py
@@ -1,16 +1,34 @@
+import random
+import string
 def jogar():
+    global palavras
     print('#' * 65)
     print('Bem vindo(a) ao jogo de forca')
     print('                        Boa sorte!!!')
     print('#' * 65)
 
-    palavra_secreta: str = 'BANANA'.lower()
+    # criar e ler arquivo
+    arquivo = open('palavras.txt','r')
+    palavras = []  # criei uma lista vazia para receber as palavras
+    #Laço para ler cada linha do arquivo
+    for linha in arquivo:
+        linha = linha.strip() # leitura linha a linha tirando espaços
+        palavras.append(linha)
+    arquivo.close()
+
+    #Gera a lista aleatóriamente, o len é útil p/ saber o tamanho da lista
+    numero: int = random.randrange(0,len(palavras))
+    palavra_secreta: str = palavras[numero].lower()
     #para apresentar os espaçamentos de forma dinâmica
     letras_acertadas: list = [' _ '] * len(palavra_secreta)
+    # também tem uma outra opção que é:
+    #letras_acertadas= [' _ ' for letra in palavra_secreta]
+    print(palavra_secreta)
 
     enforcou  = False
     acertou: bool = False
-    erros: bool = False
+    # analisa se as tentativas são do mesmo tamanho da palavra secteta
+    tentativas: int = len(palavra_secreta) -1
 
     # para iniciar o jogo mostrando a a forca
     print(letras_acertadas)
@@ -28,16 +46,19 @@ def jogar():
                     letras_acertadas[posicao] = letra
                 posicao += 1
         else:
-            erros += 1
-            # verificar se já atingiu o limite de tentativas
-        enforcou = erros == 6
+            tentativas -= 1
         # enquanto _ ñ existir dentro das letras acertadas ai pode encerrar
         acertou = ' _ ' not in letras_acertadas
+        # usa o número de letras da palavra secreta como limite de tentativas
+        enforcou = tentativas == 0
         print(letras_acertadas)
+
     if acertou:
         print(f'Parabéns, você acertou a palavra secreta é: {palavra_secreta}')
     else:
-        print(f'Não foi dessa vez, você perdeu. A palavra secreta era: {palavra_secreta}')
+        print(f'Não foi dessa vez, você perdeu.'
+              f' A palavra secreta era:'f'{palavra_secreta}')
+
     print("## Fim do jogo ##")
 
     # função necessária para rodar a classe atual, sem isso eu não consigo
